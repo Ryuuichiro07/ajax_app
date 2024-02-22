6887d590a952ad009f592365f2fbd97f237d2e57
Ryuuichiro07 <159006757+Ryuuichiro07@users.noreply.github.com>
2024-02-22 16:22:05 +0900

    詳細の記述

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -14,22 +14,36 @@ const buildHTML = (XHR) => {
 
 function post (){
   const form = document.getElementById("form");
+  // formっていうid属性をとって、formっていう定数に定義してる
   form.addEventListener("submit", (e) => {
+    // 送信ボタンの読み込み
     e.preventDefault();
+    // 無駄な処理をなくす、読み込みが多くならないように
     const formData = new FormData(form);
+    // 新規のデータを読み込む
     const XHR = new XMLHttpRequest();
+    // 新規のHTTPメソッドを読み込む
     XHR.open("POST", "/posts", true);
+    // （）内のデータを指定して読み込む
     XHR.responseType = "json";
+    // レスポンスをJSONとして送る情報をもつ
     XHR.send(formData);
+    // 送信のデータを送る
     XHR.onload = () => {
+      // onloadはリクエストの送信が成功した時に動く
       if (XHR.status != 200) {
         alert(`Error ${XHR.status}: ${XHR.statusText}`);
         return null;
       };
+      // 条件分岐、結果が２００なら実施、それ以外なら以下のコードを実施せずに元に戻す
       const list = document.getElementById("list");
+      // listのidを取得してlistに定義する
       const formText = document.getElementById("content");
+      // contentのidを取得してformtextに定義する
       list.insertAdjacentHTML("afterend", buildHTML(XHR));
+      // listの後にbuildHTMLを行い挿入する
       formText.value = "";
+      // valueを空白にする
     };
   });
 };
